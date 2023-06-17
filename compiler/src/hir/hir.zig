0b57ca4992d920cd3a627cfb3190d16b51e20c5b
sqyyy-jar <62748369+sqyyy-jar@users.noreply.github.com>
2023-06-17 13:36:25 +0200

    feat: improve basic HIR structure

--- compiler/src/hir/hir.zig
+++ compiler/src/hir/hir.zig
@@ -1,17 +1,25 @@
 const std = @import("std");
+const lexer = @import("../lexer.zig");
+
+pub const HirError = error{} || lexer.LexerError;
 
 pub const Hir = struct {
     const Self = @This();
 
     modules: std.ArrayList(HirModule),
     imports: std.ArrayList(HirImport),
-    types: std.ArrayList(HirType),
+    types: std.ArrayList(HirTypeDecl),
     functions: std.ArrayList(HirFunction),
 };
 
 // todo
+// - add generics
+pub const HirType = struct {
+    name: []const u8,
+};
+
+// todo
 pub const HirModule = struct {
-    statement: []const u8,
     name: []const u8,
 };
 
@@ -21,11 +29,56 @@ pub const HirImport = struct {
 };
 
 // todo
-pub const HirType = union(enum) {
-    trait_type: struct {},
-    struct_type: struct {},
-    enum_type: struct {},
+pub const HirTypeDecl = union(enum) {
+    trait_type: struct {
+        name: []const u8,
+        functions: std.ArrayList(HirFunction),
+    },
+    struct_type: struct {
+        name: []const u8,
+        fields: std.ArrayList(HirStructField),
+        functions: std.ArrayList(HirFunction),
+    },
+    enum_type: struct {
+        name: []const u8,
+        variants: std.ArrayList(HirEnumVariant),
+        functions: std.ArrayList(HirFunction),
+    },
 };
 
 // todo
-pub const HirFunction = struct {};
+pub const HirFunction = struct {
+    name: []const u8,
+    params: std.ArrayList(HirFunctionParam),
+    return_type: ?HirType,
+    body: ?HirBlock,
+};
+
+// todo
+pub const HirFunctionParam = struct {
+    name: []const u8,
+    type: HirType,
+};
+
+// todo
+pub const HirStructField = struct {
+    name: []const u8,
+    type: HirType,
+};
+
+// todo
+pub const HirEnumVariant = struct {
+    name: []const u8,
+    type: ?HirType,
+};
+
+// todo
+pub const HirBlock = struct {
+    statements: std.ArrayList(HirStatement),
+};
+
+// todo
+pub const HirStatement = union(enum) {
+    call: struct {},
+    _return: struct {},
+};
--- compiler/src/hir/parser.zig
+++ compiler/src/hir/parser.zig
@@ -1,2 +1,8 @@
 const lexer = @import("../lexer.zig");
 const hir = @import("./hir.zig");
+
+pub fn parse(src: []const u8) hir.HirError!hir.Hir {
+    var lex = lexer.Lexer.init(src);
+    while (true) {}
+    _ = lex;
+}
